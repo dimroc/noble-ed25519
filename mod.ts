7be4a95f2cd85b0d806bf65102756acc3501d0a9
Dimitri Roche <635121+dimroc@users.noreply.github.com>
2021-12-03 14:56:09 -0800

    change import url from denopkg to github for sha256

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 import { getPublicKey, sign, verify, utils, CURVE, Point, ExtendedPoint } from './index.ts';
-import { SHA512 } from 'https://denopkg.com/chiefbiiko/sha512/mod.ts';
+import { SHA512 } from 'https://raw.githubusercontent.com/chiefbiiko/sha512/master/mod.ts';
 
 utils.sha512 = async (message: Uint8Array): Promise<Uint8Array> => {
   return new SHA512().update(message).digest() as Uint8Array;
